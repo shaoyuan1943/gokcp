a27a21f8d14fb85cac6561fd76dcebdc633c78fa
shaoyuan <84977043@qq.com>
2020-07-01 16:09:31 +0800

    add fec

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/shaoyuan1943/gokcp
 
 go 1.13
+
+require (
+	github.com/klauspost/reedsolomon v1.9.9
+	github.com/mmcloughlin/avo v0.0.0-20200523190732-4439b6b2c061 // indirect
+)
--- pool.go
+++ pool.go
@@ -9,6 +9,11 @@ var (
 		s.dataBuffer = s.dataBuffer[:0]
 		return s
 	}}
+
+	bufferPool = sync.Pool{New: func() interface{} {
+		buffer := make([]byte, KCP_MTU_DEF)
+		return buffer
+	}}
 )
 
 func getSegment() *segment {
